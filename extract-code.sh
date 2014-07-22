81f1d3d3ee93cd34a1681ade5126aca9bc2a9e10
Martin.Knopf <Martin.Knopf@payone.de>
2014-07-22 16:51:09 +0200

    trying to extract code from book directory

--- extract-code.sh
+++ extract-code.sh
@@ -4,14 +4,14 @@ DESTINATION_DIRECTORY="$1"
 
 function extractCode {
   SOURCE_FILE_NAME=$(sed -e 's/\.md//' <<< $1)
-  SOURCE_FILE_NAME=$(sed -e 's/\/book/$2/' <<< $SOURCE_FILE_NAME)
+  SOURCE_FILE_NAME=$(sed -e "s/\/book/$2/" <<< $SOURCE_FILE_NAME)
 
   grep '^    ' $1 | sed -e 's/^    //' > $SOURCE_FILE_NAME
 }
 
 for d in $(find ./book -type d)
 do
-  DIRECTORY=$(sed -e 's/\/book/$DESTINATION_DIRECTORY/' <<< $d)
+  DIRECTORY=$(sed -e "s/\/book/$DESTINATION_DIRECTORY/" <<< $d)
   echo "------ $DIRECTORY"
   if [ ! -d "$DIRECTORY" ]; then
     mkdir $DIRECTORY
